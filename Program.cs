9e2bd6c8b06ed1af637518c1ec11fdcc0f0e9d9a
sunder2568223 <sunderyssr@gmail.com>
2023-07-13 14:25:12 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace day4ass4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+
+            string parity = (number % 2 == 0) ? "even" : "odd";
+            Console.WriteLine("The number is " + parity + ".");
+        }
+    }
+}
+
